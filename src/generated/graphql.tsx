28fcdd930f516573e8de7078ab80631dfcbd369a
Dong Hyun Choi <dongandrewchoi@gmail.com>
2022-05-24 17:01:53 -0700

    initial commit

---
+++ src/generated/graphql.tsx
@@ -0,0 +1,273 @@
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+const defaultOptions = {} as const;
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+  /** The `Upload` scalar type represents a file upload. */
+  Upload: any;
+};
+
+export enum CacheControlScope {
+  Private = 'PRIVATE',
+  Public = 'PUBLIC'
+}
+
+export type Message = {
+  __typename?: 'Message';
+  body: Scalars['String'];
+  id: Scalars['ID'];
+  user: User;
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  _?: Maybe<Scalars['Boolean']>;
+  createMessage: Message;
+  createUser: User;
+  deleteMessage: Message;
+};
+
+
+export type MutationCreateMessageArgs = {
+  body: Scalars['String'];
+  username: Scalars['ID'];
+};
+
+
+export type MutationCreateUserArgs = {
+  first_name: Scalars['String'];
+  last_name: Scalars['String'];
+  username: Scalars['ID'];
+};
+
+
+export type MutationDeleteMessageArgs = {
+  id: Scalars['ID'];
+};
+
+export type Query = {
+  __typename?: 'Query';
+  _?: Maybe<Scalars['Boolean']>;
+  message: Message;
+  messages: Array<Message>;
+  user?: Maybe<User>;
+  users?: Maybe<Array<User>>;
+};
+
+
+export type QueryMessageArgs = {
+  id: Scalars['ID'];
+};
+
+
+export type QueryUserArgs = {
+  username: Scalars['ID'];
+};
+
+export type Subscription = {
+  __typename?: 'Subscription';
+  _?: Maybe<Scalars['Boolean']>;
+  messageAdded?: Maybe<Message>;
+};
+
+
+export type SubscriptionMessageAddedArgs = {
+  username: Scalars['ID'];
+};
+
+export type User = {
+  __typename?: 'User';
+  first_name: Scalars['String'];
+  last_name: Scalars['String'];
+  messages?: Maybe<Array<Message>>;
+  username: Scalars['ID'];
+};
+
+export type GetUserMessagesQueryVariables = Exact<{
+  username: Scalars['ID'];
+}>;
+
+
+export type GetUserMessagesQuery = { __typename?: 'Query', user?: { __typename?: 'User', messages?: Array<{ __typename?: 'Message', body: string }> | null } | null };
+
+export type CreateMessageMutationVariables = Exact<{
+  username: Scalars['ID'];
+  body: Scalars['String'];
+}>;
+
+
+export type CreateMessageMutation = { __typename?: 'Mutation', createMessage: { __typename?: 'Message', id: string, body: string, user: { __typename?: 'User', username: string } } };
+
+export type CreateUserMutationVariables = Exact<{
+  username: Scalars['ID'];
+  first_name: Scalars['String'];
+  last_name: Scalars['String'];
+}>;
+
+
+export type CreateUserMutation = { __typename?: 'Mutation', createUser: { __typename?: 'User', username: string, first_name: string, last_name: string } };
+
+export type GetAllUsersQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type GetAllUsersQuery = { __typename?: 'Query', users?: Array<{ __typename?: 'User', username: string, first_name: string, last_name: string }> | null };
+
+
+export const GetUserMessagesDocument = gql`
+    query getUserMessages($username: ID!) {
+  user(username: $username) {
+    messages {
+      body
+    }
+  }
+}
+    `;
+
+/**
+ * __useGetUserMessagesQuery__
+ *
+ * To run a query within a React component, call `useGetUserMessagesQuery` and pass it any options that fit your needs.
+ * When your component renders, `useGetUserMessagesQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useGetUserMessagesQuery({
+ *   variables: {
+ *      username: // value for 'username'
+ *   },
+ * });
+ */
+export function useGetUserMessagesQuery(baseOptions: Apollo.QueryHookOptions<GetUserMessagesQuery, GetUserMessagesQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<GetUserMessagesQuery, GetUserMessagesQueryVariables>(GetUserMessagesDocument, options);
+      }
+export function useGetUserMessagesLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetUserMessagesQuery, GetUserMessagesQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<GetUserMessagesQuery, GetUserMessagesQueryVariables>(GetUserMessagesDocument, options);
+        }
+export type GetUserMessagesQueryHookResult = ReturnType<typeof useGetUserMessagesQuery>;
+export type GetUserMessagesLazyQueryHookResult = ReturnType<typeof useGetUserMessagesLazyQuery>;
+export type GetUserMessagesQueryResult = Apollo.QueryResult<GetUserMessagesQuery, GetUserMessagesQueryVariables>;
+export const CreateMessageDocument = gql`
+    mutation CreateMessage($username: ID!, $body: String!) {
+  createMessage(username: $username, body: $body) {
+    id
+    body
+    user {
+      username
+    }
+  }
+}
+    `;
+export type CreateMessageMutationFn = Apollo.MutationFunction<CreateMessageMutation, CreateMessageMutationVariables>;
+
+/**
+ * __useCreateMessageMutation__
+ *
+ * To run a mutation, you first call `useCreateMessageMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useCreateMessageMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [createMessageMutation, { data, loading, error }] = useCreateMessageMutation({
+ *   variables: {
+ *      username: // value for 'username'
+ *      body: // value for 'body'
+ *   },
+ * });
+ */
+export function useCreateMessageMutation(baseOptions?: Apollo.MutationHookOptions<CreateMessageMutation, CreateMessageMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<CreateMessageMutation, CreateMessageMutationVariables>(CreateMessageDocument, options);
+      }
+export type CreateMessageMutationHookResult = ReturnType<typeof useCreateMessageMutation>;
+export type CreateMessageMutationResult = Apollo.MutationResult<CreateMessageMutation>;
+export type CreateMessageMutationOptions = Apollo.BaseMutationOptions<CreateMessageMutation, CreateMessageMutationVariables>;
+export const CreateUserDocument = gql`
+    mutation CreateUser($username: ID!, $first_name: String!, $last_name: String!) {
+  createUser(username: $username, first_name: $first_name, last_name: $last_name) {
+    username
+    first_name
+    last_name
+  }
+}
+    `;
+export type CreateUserMutationFn = Apollo.MutationFunction<CreateUserMutation, CreateUserMutationVariables>;
+
+/**
+ * __useCreateUserMutation__
+ *
+ * To run a mutation, you first call `useCreateUserMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useCreateUserMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [createUserMutation, { data, loading, error }] = useCreateUserMutation({
+ *   variables: {
+ *      username: // value for 'username'
+ *      first_name: // value for 'first_name'
+ *      last_name: // value for 'last_name'
+ *   },
+ * });
+ */
+export function useCreateUserMutation(baseOptions?: Apollo.MutationHookOptions<CreateUserMutation, CreateUserMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<CreateUserMutation, CreateUserMutationVariables>(CreateUserDocument, options);
+      }
+export type CreateUserMutationHookResult = ReturnType<typeof useCreateUserMutation>;
+export type CreateUserMutationResult = Apollo.MutationResult<CreateUserMutation>;
+export type CreateUserMutationOptions = Apollo.BaseMutationOptions<CreateUserMutation, CreateUserMutationVariables>;
+export const GetAllUsersDocument = gql`
+    query getAllUsers {
+  users {
+    username
+    first_name
+    last_name
+  }
+}
+    `;
+
+/**
+ * __useGetAllUsersQuery__
+ *
+ * To run a query within a React component, call `useGetAllUsersQuery` and pass it any options that fit your needs.
+ * When your component renders, `useGetAllUsersQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useGetAllUsersQuery({
+ *   variables: {
+ *   },
+ * });
+ */
+export function useGetAllUsersQuery(baseOptions?: Apollo.QueryHookOptions<GetAllUsersQuery, GetAllUsersQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<GetAllUsersQuery, GetAllUsersQueryVariables>(GetAllUsersDocument, options);
+      }
+export function useGetAllUsersLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetAllUsersQuery, GetAllUsersQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<GetAllUsersQuery, GetAllUsersQueryVariables>(GetAllUsersDocument, options);
+        }
+export type GetAllUsersQueryHookResult = ReturnType<typeof useGetAllUsersQuery>;
+export type GetAllUsersLazyQueryHookResult = ReturnType<typeof useGetAllUsersLazyQuery>;
+export type GetAllUsersQueryResult = Apollo.QueryResult<GetAllUsersQuery, GetAllUsersQueryVariables>;
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,237 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { ReactComponentElement, ReactElement, useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+
+} from "@apollo/client";
+import { useCreateMessageMutation, useCreateUserMutation, useGetAllUsersQuery, useGetUserMessagesQuery } from "./generated/graphql";
+
+const client = new ApolloClient({
+  uri: "https://users-messages-gql.herokuapp.com/graphql",
+  cache: new InMemoryCache(),
+});
+
+// useGetUserMessagesQuery
+// useCreateMessageMutation
+// useCreateUserMutation
+// useGetAllUsersQuery
+interface User {
+  username?: String
+  first_name?: String
+  last_name?: String
+  messages?: [Messages]
+}
+
+
+
+function GetUsers() {
+  const { loading, error, data } = useGetAllUsersQuery();
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :</p>;
+  return (
+    <div>
+      {data?.users?.map((user:User) => (
+        <div>{user.username}</div>
+      ))}
+    </div>
+  );
+}
+
+//  const ADD_USER = gql`
+//  mutation CreateUser($username: ID!, $first_name: String!, $last_name: String!) {
+// createUser(username: $username, first_name: $first_name, last_name: $last_name) {
+//  username
+//  first_name
+//  last_name
+// }
+// }
+//  `
+
+//  const ADD_MESSAGE = 
+//  gql`
+//  mutation CreateMessage($username: ID!, $body: String!) {
+// createMessage(username: $username, body: $body) {
+//  id
+//  body
+//  user {
+//    username
+//  }
+// }
+// }`
+
+
+//  const GET_MESSAGES =  gql`
+//  query getAllUsers {
+// users {
+//  username
+//  first_name
+//  last_name
+// }
+// }
+//  `
+function AddUser() {
+
+  const initialFormData = {
+    usernameInput : "",
+    firstNameInput : "",
+    lastNameInput : ""
+  }
+
+  const [formData, setFormData] = useState(initialFormData)
+
+  const [addUser, { data, loading, error }] = useCreateUserMutation();
+
+  // if (loading) return 'Submitting...';
+  // if (error) {
+  //   return `Submission error! ${error.message}`;
+  // }
+
+  function handleSubmit(event:React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    addUser({
+      variables: {
+        username: formData.usernameInput,
+        first_name: formData.firstNameInput, last_name: formData.lastNameInput
+      }
+    });
+    setFormData(initialFormData);
+  }
+
+  function handleChange(event:React.ChangeEvent<HTMLInputElement>) {
+    const {name, value} = event.target;
+
+    setFormData(fData => ({...fData, [name] : value}))
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h2>Add New User</h2>
+        <input
+          onChange={handleChange}
+          value={formData.usernameInput}
+          name="usernameInput"
+          placeholder="username"
+        />
+        <input
+          onChange={handleChange}
+          value={formData.firstNameInput}
+          name="firstNameInput"
+          placeholder="first name"
+        />
+        <input
+          onChange={handleChange}
+          value={formData.lastNameInput}
+          name="lastNameInput"
+          placeholder="last name"
+        />
+        <button type="submit">Add User</button>
+      </form>
+    </div>
+  );
+}
+
+
+function AddMessage() {
+
+  const initialFormData = {
+    usernameInput : "",
+    bodyInput : ""
+  }
+
+  const [formData, setFormData] = useState(initialFormData)
+
+  const [addUser, { data, loading, error }] = useCreateMessageMutation();
+
+  // if (loading) return 'Submitting...';
+  // if (error) {
+  //   return `Submission error! ${error.message}`;
+  // }
+
+  function handleSubmit(event:React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    addUser({
+      variables: {
+        username: formData.usernameInput,
+        body: formData.bodyInput
+      }
+    });
+    setFormData(initialFormData);
+  }
+
+  function handleChange(event:React.ChangeEvent<HTMLInputElement>) {
+    const {name, value} = event.target;
+
+    setFormData(fData => ({...fData, [name] : value}))
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h2>Add New Message</h2>
+        <input
+          onChange={handleChange}
+          value={formData.usernameInput}
+          name="usernameInput"
+          placeholder="username"
+        />
+        <input
+          onChange={handleChange}
+          value={formData.bodyInput}
+          name="bodyInput"
+          placeholder="message body"
+        />
+        <button type="submit">Add Message</button>
+      </form>
+    </div>
+  );
+}
+
+
+
+interface Messages {
+  id? : String
+  body?: String
+}
+interface username {
+  username: string
+}
+
+function GetMessages({ username }:username) {
+  const { loading, error, data } = useGetUserMessagesQuery( {
+    variables: { username },
+  });
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :</p>;
+  return (
+    <div>
+      {data?.user?.messages?.map((message:Messages) => (
+        <div>{message.body}</div>
+      ))}
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  <ApolloProvider client={client}>
+
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </ApolloProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+export { GetUsers, GetMessages, AddUser, AddMessage };
